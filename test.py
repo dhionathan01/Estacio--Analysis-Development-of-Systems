ab4e22b6ca76381aab35ed0a0b9388820a27be04
Dhionathan Jobim <dhionathan01@hotmail.com>
2020-12-10 03:26:47 -0300

    Deletar test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-print("*******************************")
-print("          Exercício 01         ")
-print("*******************************")
-numero1 = float(input("Digite um Número: "))
-numero2 = float(input("Digite um Número: "))
-numero3 = float(input("Digite um Número: "))
-valor = (numero1+numero2+numero3) / 3
-print(valor)
